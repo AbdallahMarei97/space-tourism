cc0a62de0ba51a3de05083cffe514553cb25a146
AbdallahMarei97 <abdallah.marei@optimumpartners.co>
2022-10-13 12:16:57 +0300

    task done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import './navbar.scss'
 import navbarLinks from './navbarLinks.json'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {burgerActive: false};
     }
+
+    // checkNavbarLinks = () => {
+    //     navbarLinks.forEach(link => {
+    //         if (window.location.pathname === link.url) {
+    //             this.setState({pageUrl: link.url})
+    //         }
+    //     })
+    // }
+    // componentDidMount() {
+    //     window.onpopstate = () => {
+    //         this.checkNavbarLinks()
+    //     }
+    //     this.checkNavbarLinks()
+    // }
 
     openBurgerMenu = () => {
         this.setState({burgerActive: !this.state.burgerActive})
@@ -16,18 +30,18 @@ class Navbar extends React.Component {
     render() {
         return (
           <nav className='navbar'>
-            <Link to='/home' className='logo'>
+            <NavLink to='/home' className='logo'>
                 <img src='/images/shared/logo.svg' alt='logo'/>
-            </Link>
+            </NavLink>
             <div className='burger-menu' onClick={this.openBurgerMenu}>
                 {this.state.burgerActive ? <img src='/images/shared/icon-close.svg' alt='close-icon'/> : <img src='/images/shared/icon-hamburger.svg' alt='burger-icon'/>}
             </div>
             <ul className={`${this.state.burgerActive ? 'navbar-links-burger-active' : ''} navbar-links`}>
                 {navbarLinks.map(link => (
-                <li key={link.id} className={`${window.location.pathname === link.url ? 'navbar-link-active' : ''} navbar-singular-link`}>
-                    <Link to={link.url} onClick={()=>this.setState({...this.state, pageUrl: link.url})}>
+                <li key={link.id} className='navbar-singular-link'>
+                    <NavLink to={link.url}>
                         <span className='navbar-link-bold'>{link.linkNumber}</span> {link.linkName}
-                    </Link>
+                    </NavLink>
                 </li>
                 ))}
             </ul>
